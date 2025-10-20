e3df2b4d37472cd967103e4d6e41fd07c1bd6f50
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2025-10-20 15:42:48 -0300

    Barra de menu quase terminada.

---
+++ JS/funcoes.js
@@ -0,0 +1,11 @@
+// Abre o menu
+function abrirMenu()
+{
+    document.getElementById("menuLateral").style.width = "250px";
+}
+
+// Fecha o menu
+function fecharMenu()
+{
+    document.getElementById("menuLateral").style.width = "0";
+}
