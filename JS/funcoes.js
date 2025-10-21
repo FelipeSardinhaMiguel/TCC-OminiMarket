473cc8a4ada5b39425b15e885f6890bc2357527d
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2025-10-21 16:35:03 -0300

    Nav bar concluida.

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -1,11 +1,20 @@
-// Abre o menu
 function abrirMenu()
 {
     document.getElementById("menuLateral").style.width = "250px";
 }
 
-// Fecha o menu
 function fecharMenu()
 {
     document.getElementById("menuLateral").style.width = "0";
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const subMenu = document.querySelector(".subMenu");
+    const botaoProdutos = subMenu.querySelector(".produtosItens");
+
+    botaoProdutos.addEventListener("click", (e) => {
+        e.preventDefault(); // impede o link de recarregar a página
+        subMenu.classList.toggle("aberto"); // alterna entre mostrar e esconder
+    });
+});
