732ab8de8492713c66f781358bb19cc6af5c7682
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2025-10-23 17:41:24 -0300

    Barra de pesquisa.

--- JS/entrou.js
+++ JS/entrou.js
@@ -1,3 +1,23 @@
+//Funções da navbar
+/*function abrirMenu()
+{
+    document.getElementById("menuLateral").style.width = "250px";
+}
+    
+function fecharMenu()
+{
+    document.getElementById("menuLateral").style.width = "0";
+}
+        
+document.addEventListener("DOMContentLoaded", () => {
+    const subMenu = document.querySelector(".subMenu");
+    const botaoProdutos = subMenu.querySelector(".produtosItens");
+
+    botaoProdutos.addEventListener("click", (e) => {
+    e.preventDefault(); // impede o link de recarregar a página
+    subMenu.classList.toggle("aberto"); // alterna entre mostrar e esconder
+    });
+});*/
 function entrar()
 {
     alert("Erro ao entrar na conta. Confira sua senha e e-mail.");
